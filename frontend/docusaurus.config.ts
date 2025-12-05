0740b6c9d8e7c4151fadfad187663ef93fdfe503
Maryam Jamil <154235773+MaryamJami1@users.noreply.github.com>
2025-12-05 11:44:08 +0500

    Change base URL in Docusaurus config

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://MaryamJami1.github.io',
-  baseUrl: '/Robotics_book_chatbot/',
+  baseUrl: '/',
   organizationName: 'MaryamJami1',
   projectName: 'Robotics_book_chatbot',
   deploymentBranch: 'gh-pages',
